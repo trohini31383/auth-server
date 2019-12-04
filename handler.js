72909d11a22f7612ab07cdb03bed8f0baff820f5
trohini31383 <53841712+trohini31383@users.noreply.github.com>
2019-12-04 10:04:45 -0800

    updated

--- handler.js
+++ handler.js
@@ -2,6 +2,7 @@
 
 const axios = require('axios');
 
+
 module.exports.getAccessToken = async (event) => {
 
   const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
@@ -9,12 +10,15 @@ module.exports.getAccessToken = async (event) => {
     + '&client_secret=5uq2hv63r35ev80iu3oj53ljse'
     + '&grant_type=authorization_code'
     + '&redirect_uri=https://trohini31383.github.io/meetup/'
-    + '&code=https://trohini31383.github.io/meetup/?code=25ca98e9884035e367ba608888bbff59';
+    + '&code=' + event.pathParameters.code;
 
   const info = await axios.post(MEETUP_OAUTH_URL);
 
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
     body: JSON.stringify({
       access_token: info.data.access_token,
       refresh_token: info.data.refresh_token,
