7dca0f0a8700a32f6145f5b54d34b98e981ae082
trohini31383 <53841712+trohini31383@users.noreply.github.com>
2019-12-04 16:20:31 -0800

    updated refreshToken

--- handler.js
+++ handler.js
@@ -25,3 +25,31 @@ module.exports.getAccessToken = async (event) => {
     }),
   };
 };
+
+module.exports.refreshAccessToken = async (event) => {
+
+  const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
+    + '?client_id=i0ng01ifl76kt2foju7hisn46j'
+    + '&client_secret=5uq2hv63r35ev80iu3oj53ljse'
+    + '&grant_type=refresh_token'
+    + '&refresh_token=' + event.pathParameters.refresh_token
+
+
+
+  const info = await axios.post(MEETUP_OAUTH_URL);
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token,
+    }),
+  };
+};
+
+
+
+
